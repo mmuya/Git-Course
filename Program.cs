68c2dfbb82263e0f8da54cdf309d24d2c1dea1d4
Mike Muya <muyatshib@gmail.com>
2018-03-21 15:28:19 -0300

    Acrescentada o bom dia

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia, como você está hoje? ");
+            Console.ReadLine();
         }
     }
 }
